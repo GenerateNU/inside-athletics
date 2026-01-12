8988d77470861ab206197f7818c94470d42a4f6b
Suli Rashidzada <secretyogurt@Sulis-MacBook-Air.local>
2026-01-12 17:22:18 -0500

    merge conflict resolution

--- backend/internal/tests/goat_test.go
+++ backend/internal/tests/goat_test.go
@@ -1,8 +1,9 @@
 package tests
 
 import (
-	"testing"
+	"fmt"
 	"inside-athletics/internal/models"
+	"testing"
 )
 
 func TestGoat(t *testing.T) {
@@ -13,4 +14,22 @@ func TestGoat(t *testing.T) {
 	if actual != expected {
 		t.Error("The goat did not make some noise...")
 	}
+}
+
+func TestPointers(t *testing.T) {
+	goat := models.Goat{Id: 1, Name: "Joe", Age: 67}
+	goat.SetName("Suli")
+	if goat.Name != "Suli" {
+		t.Error("Set name didn't work...")
+	}
+	goat.SetNameCopy("Erm")
+	if goat.Name != "Suli" {
+		t.Error("I was wrong and it didn't use a copy")
+	}
+
+	copy := goat.MakeCopy()
+	if &copy == &goat {
+		t.Error("THEY ARE THE SAME OH MAN")
+	}
+	fmt.Printf("Copy pointer: %p, OG pointer: %p", &copy, &goat)
 }
