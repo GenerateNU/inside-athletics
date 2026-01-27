985ee8bc0b02d9470e123eefaa3102e56e32ab14
tGiech22 <tonygiech@gmail.com>
2026-01-27 17:46:38 -0500

    fix: adding default case

--- backend/internal/utils/error_handling.go
+++ backend/internal/utils/error_handling.go
@@ -15,26 +15,29 @@ func handleGORMErrors(err error) error {
 	if err != nil {
 		switch {
 		case errors.Is(err, gorm.ErrRecordNotFound):
-        	return huma.Error404NotFound("Resource not found")
-    	case errors.Is(err, gorm.ErrDuplicatedKey):
-        	return huma.Error409Conflict("Resource already exists", err)
-   		case errors.Is(err, gorm.ErrInvalidData), errors.Is(err, gorm.ErrInvalidField), errors.Is(err, gorm.ErrInvalidValue), errors.Is(err, gorm.ErrInvalidValueOfLength):
-        	return huma.Error400BadRequest("Invalid data provided", err)
-    	case errors.Is(err, gorm.ErrMissingWhereClause):
-        	return huma.Error400BadRequest("Missing required filter conditions")
-   		case errors.Is(err, gorm.ErrPrimaryKeyRequired):
-        	return huma.Error400BadRequest("Primary key is required", err)
-    	case errors.Is(err, gorm.ErrInvalidTransaction):
-        	return huma.Error500InternalServerError("Transaction error", err)
-    	case errors.Is(err, gorm.ErrUnsupportedDriver), errors.Is(err, gorm.ErrNotImplemented):
-        	return huma.Error501NotImplemented("Operation not supported", err)
+			return huma.Error404NotFound("Resource not found")
+		case errors.Is(err, gorm.ErrDuplicatedKey):
+			return huma.Error409Conflict("Resource already exists", err)
+		case errors.Is(err, gorm.ErrInvalidData), errors.Is(err, gorm.ErrInvalidField), errors.Is(err, gorm.ErrInvalidValue), errors.Is(err, gorm.ErrInvalidValueOfLength):
+			return huma.Error400BadRequest("Invalid data provided", err)
+		case errors.Is(err, gorm.ErrMissingWhereClause):
+			return huma.Error400BadRequest("Missing required filter conditions")
+		case errors.Is(err, gorm.ErrPrimaryKeyRequired):
+			return huma.Error400BadRequest("Primary key is required", err)
+		case errors.Is(err, gorm.ErrInvalidTransaction):
+			return huma.Error500InternalServerError("Transaction error", err)
+		case errors.Is(err, gorm.ErrUnsupportedDriver), errors.Is(err, gorm.ErrNotImplemented):
+			return huma.Error501NotImplemented("Operation not supported", err)
+
+		default:
+			return huma.Error500InternalServerError("Database error", err) // default case
 		}
-		return huma.Error500InternalServerError("Database error", err)
 	}
 	return nil
 }
 
-/**
+/*
+*
 Handles case when an error is returned by the DB. Ensures that nil is returned
 instead of an empty entity struct
 */
