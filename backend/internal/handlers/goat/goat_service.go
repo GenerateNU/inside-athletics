4569f535c1d60761ee630be7aabddc4535819915
Hannah Bang <65321838+hannahbang1108@users.noreply.github.com>
2026-01-29 19:08:52 -0500

    replaced fmt with human errors

--- backend/internal/handlers/goat/goat_service.go
+++ backend/internal/handlers/goat/goat_service.go
@@ -1,39 +1,142 @@
-package goat
+package sport
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"inside-athletics/internal/utils"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
-type GoatService struct {
-	goatDB *GoatDB // give the service a DB connection!
+type SportService struct {
+	sportDB *SportDB
 }
 
-func (g *GoatService) Ping(ctx context.Context, input *utils.EmptyInput) (*utils.ResponseBody[GoatResponse], error) {
-	resp := &GoatResponse{
-		Id:   1,
-		Name: "Suli",
-		Age:  67,
+// NewSportService creates a new SportService instance
+func NewSportService(db *gorm.DB) *SportService {
+	return &SportService{
+		sportDB: NewSportDB(db),
 	}
-	return &utils.ResponseBody[GoatResponse]{
-		Body: resp,
+}
+
+func (s *SportService) CreateSport(ctx context.Context, input *struct{ Body CreateSportRequest }) (*utils.ResponseBody[SportResponse], error) {
+	// Validate business rules
+	if input.Body.Name == "" {
+		return nil, huma.Error422UnprocessableEntity("name cannot be empty")
+	}
+	if input.Body.Popularity != nil && *input.Body.Popularity < 0 {
+		return nil, huma.Error422UnprocessableEntity("popularity cannot be negative")
+	}
+
+	sport, err := s.sportDB.CreateSport(input.Body.Name, input.Body.Popularity)
+	if err != nil {
+		return nil, err
+	}
+
+	return &utils.ResponseBody[SportResponse]{
+		Body: ToSportResponse(sport),
 	}, nil
 }
 
-func (g *GoatService) GetGoat(ctx context.Context, input *GetGoatParams) (*utils.ResponseBody[GoatResponse], error) {
-	resp := &utils.ResponseBody[GoatResponse]{}
-	id := input.Id
-	goat, err := g.goatDB.GetGoat(uint(id))
-
-	if err != nil { // if we have an err getting the goat return the empty response and the error
-		return resp, err
+func (s *SportService) GetSportByName(ctx context.Context, input *GetSportByNameParams) (*utils.ResponseBody[SportResponse], error) {
+	sport, err := s.sportDB.GetSportByName(input.Name)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, huma.Error404NotFound("sport not found")
+		}
+		return nil, err
 	}
 
-	resp.Body = &GoatResponse{
-		Id:   int8(id),
-		Name: goat.Name,
-		Age:  goat.Age,
+	return &utils.ResponseBody[SportResponse]{
+		Body: ToSportResponse(sport),
+	}, nil
+}
+
+func (s *SportService) GetAllSports(ctx context.Context, input *GetAllSportsParams) (*utils.ResponseBody[GetAllSportsResponse], error) {
+	sports, total, err := s.sportDB.GetAllSports(input.Limit, input.Offset)
+	if err != nil {
+		return nil, err
 	}
 
-	return resp, nil
+	sportResponses := make([]SportResponse, 0, len(sports))
+	for i := range sports {
+		sportResponses = append(sportResponses, *ToSportResponse(&sports[i]))
+	}
+
+	return &utils.ResponseBody[GetAllSportsResponse]{
+		Body: &GetAllSportsResponse{
+			Sports: sportResponses,
+			Total:  int(total),
+		},
+	}, nil
+}
+
+func (s *SportService) GetSportByID(ctx context.Context, input *GetSportByIDParams) (*utils.ResponseBody[SportResponse], error) {
+	sport, err := s.sportDB.GetSportByID(input.ID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, huma.Error404NotFound("sport not found")
+		}
+		return nil, err
+	}
+
+	return &utils.ResponseBody[SportResponse]{
+		Body: ToSportResponse(sport),
+	}, nil
+}
+
+func (s *SportService) UpdateSport(ctx context.Context, input *struct {
+	ID   uuid.UUID `path:"id"`
+	Body UpdateSportRequest
+}) (*utils.ResponseBody[SportResponse], error) {
+	sport, err := s.sportDB.GetSportByID(input.ID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, huma.Error404NotFound("sport not found")
+		}
+		return nil, err
+	}
+
+	// Apply partial updates
+	if input.Body.Name != nil {
+		if *input.Body.Name == "" {
+			return nil, huma.Error422UnprocessableEntity("name cannot be empty")
+		}
+		sport.Name = *input.Body.Name
+	}
+
+	if input.Body.Popularity != nil {
+		if *input.Body.Popularity < 0 {
+			return nil, huma.Error422UnprocessableEntity("popularity cannot be negative")
+		}
+		sport.Popularity = input.Body.Popularity
+	}
+
+	updatedSport, err := s.sportDB.UpdateSport(sport)
+	if err != nil {
+		return nil, err
+	}
+
+	return &utils.ResponseBody[SportResponse]{
+		Body: ToSportResponse(updatedSport),
+	}, nil
+}
+
+func (s *SportService) DeleteSport(ctx context.Context, input *DeleteSportRequest) (*utils.ResponseBody[map[string]string], error) {
+	err := s.sportDB.DeleteSport(input.ID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, huma.Error404NotFound("sport not found")
+		}
+		return nil, err
+	}
+
+	return &utils.ResponseBody[map[string]string]{
+		Body: &map[string]string{
+			"message": "Sport deleted successfully",
+		},
+	}, nil
 }
