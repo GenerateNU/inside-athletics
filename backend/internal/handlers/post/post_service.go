a3e5a803dae4ed2dc6874158f58574e113c88039
Hannah Bang <hannahbang@outlook.com>
2026-01-31 16:38:25 -0500

    started on service

---
+++ backend/internal/handlers/post/post_service.go
@@ -0,0 +1,49 @@
+package post
+
+import (
+	"context"
+	"inside-athletics/internal/utils"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type PostService struct {
+	postDB *postDB
+}
+
+// NewPostService creates a new PostService instance
+func NewPostService(db *gorm.DB) *PostService {
+	return &PostService{
+		PostDB: NewPostDB(db),
+	}
+}
+
+func (s *PostService) CreatePost(ctx context.Context, input *struct{ Body CreatePostRequest }) (*utils.ResponseBody[PostResponse], error) {
+	// Validate business rules
+	if isNil(input.Body.AuthorId) {
+		return nil, huma.Error422UnprocessableEntity("Author ID cannot be null")
+	}
+	if isNil(input.Body.SportId) {
+		return nil, huma.Error422UnprocessableEntity("Sport ID cannot be null")
+	}
+	if input.Body.Title == "" {
+		return nil, huma.Error422UnprocessableEntity("Title cannot be empty")
+	}
+	if input.Body.Content == "" {
+		return nil, huma.Error422UnprocessableEntity("Content cannot be empty")
+	}
+	if input.Body.isAnonymous == nil {
+		return nil, huma.Error422UnprocessableEntity("isAnonymous cannot be null")
+	}
+
+	post, err := utils.HandleDBError(s.postDB.CreatePost(input.Body.AuthorId, input.Body.SportId, input.Body.Title, input.Body.Content, input.Body.isAnonymous))
+	if err != nil {
+		return nil, err
+	}
+
+	return &utils.ResponseBody[PostResponse]{
+		Body: ToPostResponse(post),
+	}, nil
+}
