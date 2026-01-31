acd49d24dc212b39903ab474bba167356ea8111c
P4rz1val22 <luis.sar.cor@gmail.com>
2026-01-31 16:41:10 -0500

    IsAnanymous

--- backend/internal/handlers/post/post_db.go
+++ backend/internal/handlers/post/post_db.go
@@ -26,7 +26,7 @@ func (s *PostDB) CreatePost(uuid.UUID author_id, uuid.UUID sport_id, ) (*models.
 		Content    string         `json:"content" example:"My name is Bob Joe and I am a rising senior who just got into NEU. What is the fencing program like? Are they competitive?" gorm:"type:varchar(5000);not null" validate:"required,min=1,max=5000"`
 		UpVotes	   int64          `json:"numUpVotes,omitempty" example:"20000" gorm:"type:int"`
 		DownVotes  int64          `json:"numDownVotes,omitempty" example:"20000" gorm:"type:int"`
-		IsAnonymous bool          `json:"isAnanymous"`
+		IsAnonymous bool          `json:"isAnonymous"`
 	}
 	dbResponse := s.db.Create(&sport)
 	return utils.HandleDBError(&sport, dbResponse.Error) 
