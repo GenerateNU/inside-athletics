43e147e5aa52552c82231de8af49a20391e5a068
Hannah Bang <hannahbang@outlook.com>
2026-01-31 16:09:51 -0500

    wrote post.go, post_route.go

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -2,7 +2,7 @@ package server
 
 import (
 	"encoding/json"
-	"inside-athletics/internal/handlers/goat"
+	"inside-athletics/internal/handlers/post"
 	"inside-athletics/internal/handlers/health"
 	"inside-athletics/internal/handlers/user"
 	"strings"
@@ -57,7 +57,7 @@ func CreateApp(db *gorm.DB) *App {
 
 func CreateRoutes(db *gorm.DB, api huma.API) {
 	// Create all the routing groups:
-	routeGroups := [...]RouteFN{health.Route, user.Route, goat.Route}
+	routeGroups := [...]RouteFN{health.Route, user.Route, post.Route}
 	for _, fn := range routeGroups {
 		fn(api, db)
 	}
