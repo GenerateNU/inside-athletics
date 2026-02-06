92b44b54cdae940ac9a37e30478b699bd7859d01
Suli Rashidzada <secretyogurt@Sulis-MacBook-Air.local>
2026-02-05 20:13:37 -0500

    created file outline and env key

---
+++ backend/internal/handlers/stripe/stripe_route.go
@@ -0,0 +1,10 @@
+package stripe
+
+import (
+	"github.com/danielgtaylor/huma/v2"
+	"gorm.io/gorm"
+)
+
+func Route(api huma.API, db *gorm.DB) {
+
+}
---
+++ backend/internal/handlers/stripe/stripe_service.go
@@ -0,0 +1,3 @@
+package stripe
+
+type StripeService struct{}
