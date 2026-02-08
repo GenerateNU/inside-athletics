7cea958956e49b997b59ef0ddd77212cd238036b
Hannah Bang <hannahbang@outlook.com>
2026-02-08 11:19:31 -0500

    added routes

--- backend/internal/handlers/stripe/stripe_route.go
+++ backend/internal/handlers/stripe/stripe_route.go
@@ -6,5 +6,12 @@ import (
 )
 
 func Route(api huma.API, db *gorm.DB) {
-
+	stripeService := NewStripeService(db)
+	{
+		grp := huma.NewGroup(api, "/api/v1/stripe")
+		huma.Post(grp, "/", stripeService.CreateProduct)                // Create product (subscription plan)
+		huma.Get(grp, "/{id}", stripeService.GetProdcutByID)            // Read product by ID
+		huma.Patch(grp, "/{id}", stripeService.UpdateProduct)           // Update product
+		huma.Delete(grp, "/{id}", stripeService.DeleteProduct)          // Delete product
+	}
 }
