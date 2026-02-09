9315fe5f988250f11c9917a2990a305f5a2b4001
Avni Mishra <avni.mishra0409@gmail.com>
2026-02-09 17:02:57 -0500

    Adding customer crud interaction

--- backend/internal/handlers/stripe/stripe_service.go
+++ backend/internal/handlers/stripe/stripe_service.go
@@ -1,3 +1,144 @@
 package stripe
 
+import (
+	"context"
+	"inside-athletics/internal/utils"
+
+	"github.com/google/uuid"
+	"github.com/stripe/stripe-go/v81"
+	"github.com/stripe/stripe-go/v81/customer"
+)
+
 type StripeService struct{}
+
+func (s *StripeService) RegisterStripeCustomer(ctx context.Context, input *RegisterStripeCustomerInput) (*utils.ResponseBody[RegisterStripeCustomerResponse], error) {
+	params := &stripe.CustomerParams{
+		Name:        input.Body.Name,
+		Email:       input.Body.Email,
+		Phone:       input.Body.Phone,
+		Description: input.Body.Description,
+	}
+
+	result, err := customer.New(params)
+	if err != nil {
+		return nil, err
+	}
+	return &utils.ResponseBody[RegisterStripeCustomerResponse]{
+		Body: &RegisterStripeCustomerResponse{
+			ID: uuid.MustParse(result.ID),
+		},
+	}, nil
+}
+
+func (s *StripeService) GetStripeCustomer(ctx context.Context, input *GetStripeCustomerInput) (*utils.ResponseBody[GetStripeCustomerResponse], error) {
+	id := input.ID
+
+	params := &stripe.CustomerParams{}
+	result, err := customer.Get(id.String(), params)
+	if err != nil {
+		return nil, err
+	}
+
+	return &utils.ResponseBody[GetStripeCustomerResponse]{
+		Body: mapStripeCustomerToModel(result),
+	}, nil
+}
+
+func (s *StripeService) UpdateStripeCustomer(ctx context.Context, input *UpdateStripeCustomerInput) (*utils.ResponseBody[GetStripeCustomerResponse], error) {
+	id := input.ID
+
+	params := &stripe.CustomerParams{
+		Name:          input.Body.Name,
+		Email:         input.Body.Email,
+		Phone:         input.Body.Phone,
+		Description:   input.Body.Description,
+		DefaultSource: input.Body.DefaultSource,
+		Balance:       input.Body.Balance,
+		Coupon:        input.Body.Coupon,
+		PromotionCode: input.Body.PromotionCode,
+	}
+
+	if input.Body.TaxExempt != nil {
+		params.TaxExempt = stripe.String(*input.Body.TaxExempt)
+	}
+
+	if input.Body.Metadata != nil {
+		for k, v := range input.Body.Metadata {
+			params.AddMetadata(k, v)
+		}
+	}
+
+	result, err := customer.Get(id.String(), params)
+	if err != nil {
+		return nil, err
+	}
+	return &utils.ResponseBody[GetStripeCustomerResponse]{
+		Body: mapStripeCustomerToModel(result),
+	}, nil
+}
+
+func (s *StripeService) DeleteStripeCustomer(ctx context.Context, input *DeleteStripeCustomerInput) (*utils.ResponseBody[DeleteStripeCustomerResponse], error) {
+	id := input.ID
+
+	params := &stripe.CustomerParams{}
+
+	results, err := customer.Del(id.String(), params)
+	if err != nil {
+		return nil, err
+	}
+	respBody := &DeleteStripeCustomerResponse{
+		ID:      results.ID,
+		Object:  results.Object,
+		Deleted: results.Deleted,
+	}
+	return &utils.ResponseBody[DeleteStripeCustomerResponse]{
+		Body: respBody,
+	}, nil
+}
+
+func mapStripeCustomerToModel(c *stripe.Customer) *GetStripeCustomerResponse {
+	customer := &GetStripeCustomerResponse{
+		ID:                  c.ID,
+		Object:              string(c.Object),
+		Balance:             c.Balance,
+		Created:             c.Created,
+		Delinquent:          c.Delinquent,
+		Email:               c.Email,
+		InvoicePrefix:       c.InvoicePrefix,
+		Livemode:            c.Livemode,
+		Metadata:            c.Metadata,
+		NextInvoiceSequence: int(c.NextInvoiceSequence),
+		TaxExempt:           string(c.TaxExempt),
+	}
+
+	// Map optional fields
+	if c.DefaultSource != nil && c.DefaultSource.ID != "" {
+		customer.DefaultSource = &c.DefaultSource.ID
+	}
+	if c.Description != "" {
+		customer.Description = &c.Description
+	}
+	if c.Name != "" {
+		customer.Name = &c.Name
+	}
+	if c.Phone != "" {
+		customer.Phone = &c.Phone
+	}
+	if len(c.PreferredLocales) > 0 {
+		customer.PreferredLocales = c.PreferredLocales
+	}
+
+	// Map invoice settings
+	if c.InvoiceSettings != nil {
+		customer.InvoiceSettings = &InvoiceSettings{}
+		if c.InvoiceSettings.DefaultPaymentMethod != nil {
+			pm := c.InvoiceSettings.DefaultPaymentMethod.ID
+			customer.InvoiceSettings.DefaultPaymentMethod = &pm
+		}
+		if c.InvoiceSettings.Footer != "" {
+			customer.InvoiceSettings.Footer = &c.InvoiceSettings.Footer
+		}
+	}
+
+	return customer
+}
--- backend/internal/handlers/stripe/stripe_types.go
+++ backend/internal/handlers/stripe/stripe_types.go
@@ -1,1 +1,87 @@
 package stripe
+
+import "github.com/google/uuid"
+
+type GetStripeCustomerInput struct {
+	ID uuid.UUID `path:"id" maxLength:"36" example:"1" doc:"ID to identify the stripe user"`
+}
+
+type GetStripeCustomerResponse struct {
+	ID                  string            `gorm:"primaryKey;column:id" json:"id" example:"cus_NffrFeUfNV2Hib" doc:"Stripe customer ID"`
+	Object              string            `gorm:"column:object" json:"object" example:"customer" doc:"Object type"`
+	Address             *string           `gorm:"column:address;type:jsonb" json:"address" doc:"Customer address"`
+	Balance             int64             `gorm:"column:balance;default:0" json:"balance" example:"0" doc:"Customer balance in cents"`
+	Created             int64             `gorm:"column:created" json:"created" example:"1680893993" doc:"Unix timestamp of creation"`
+	Currency            *string           `gorm:"column:currency" json:"currency" doc:"Default currency"`
+	DefaultSource       *string           `gorm:"column:default_source" json:"default_source" doc:"Default payment source ID"`
+	Delinquent          bool              `gorm:"column:delinquent;default:false" json:"delinquent" example:"false" doc:"Whether customer is delinquent"`
+	Description         *string           `gorm:"column:description" json:"description" doc:"Customer description"`
+	Email               string            `gorm:"column:email" json:"email" example:"jennyrosen@example.com" doc:"Customer email"`
+	InvoicePrefix       string            `gorm:"column:invoice_prefix" json:"invoice_prefix" example:"0759376C" doc:"Invoice number prefix"`
+	InvoiceSettings     *InvoiceSettings  `gorm:"column:invoice_settings;type:jsonb;serializer:json" json:"invoice_settings" doc:"Invoice settings"`
+	Livemode            bool              `gorm:"column:livemode;default:false" json:"livemode" example:"false" doc:"Whether in live mode"`
+	Metadata            map[string]string `gorm:"column:metadata;type:jsonb;serializer:json" json:"metadata" doc:"Custom metadata"`
+	Name                *string           `gorm:"column:name" json:"name" example:"Jenny Rosen" doc:"Customer name"`
+	NextInvoiceSequence int               `gorm:"column:next_invoice_sequence;default:1" json:"next_invoice_sequence" example:"1" doc:"Next invoice sequence number"`
+	Phone               *string           `gorm:"column:phone" json:"phone" doc:"Customer phone number"`
+	PreferredLocales    []string          `gorm:"column:preferred_locales;type:jsonb;serializer:json" json:"preferred_locales" doc:"Preferred locales"`
+	Shipping            *string           `gorm:"column:shipping;type:jsonb" json:"shipping" doc:"Shipping information"`
+	TaxExempt           string            `gorm:"column:tax_exempt;default:'none'" json:"tax_exempt" example:"none" doc:"Tax exempt status"`
+	TestClock           *string           `gorm:"column:test_clock" json:"test_clock" doc:"Test clock ID"`
+}
+
+type InvoiceSettings struct {
+	CustomFields         *string `json:"custom_fields"`
+	DefaultPaymentMethod *string `json:"default_payment_method"`
+	Footer               *string `json:"footer"`
+	RenderingOptions     *string `json:"rendering_options"`
+}
+
+type RegisterStripeCustomerInput struct {
+	Body RegisterStripeCustomerBody
+}
+
+type RegisterStripeCustomerBody struct {
+	Name        *string `json:"name" example:"Suli" doc:"The name of the user"`
+	Email       *string `json:"email" example:"suli@northeastern.edu" doc:"The email of the user"`
+	Phone       *string `json:"phone" example:"(111) 222-3333" doc:"The phone number of the user"`
+	Description *string `json:"description" example:"A verified athelete" doc:"A description of the user"`
+}
+
+type RegisterStripeCustomerResponse struct {
+	ID uuid.UUID `json:"id" example:"1" doc:"ID of the user on stripe"`
+}
+
+type UpdateStripeCustomerInput struct {
+	ID   uuid.UUID `path:"id" maxLength:"36" example:"1" doc:"ID to identify the stripe user"`
+	Body UpdateStripeCustomerBody
+}
+
+type UpdateStripeCustomerBody struct {
+	Balance          *int64            `json:"balance,omitempty" doc:"Current balance in cents (can be negative)"`
+	Coupon           *string           `json:"coupon,omitempty" doc:"Coupon ID to apply discount"`
+	DefaultSource    *string           `json:"default_source,omitempty" doc:"ID of default payment source"`
+	Description      *string           `json:"description,omitempty" doc:"Description of customer"`
+	Email            *string           `json:"email,omitempty" example:"newemail@example.com" doc:"Customer email"`
+	Metadata         map[string]string `json:"metadata,omitempty" doc:"Set of key-value pairs for metadata"`
+	Name             *string           `json:"name,omitempty" example:"Jane Doe" doc:"Customer name"`
+	Phone            *string           `json:"phone,omitempty" example:"+15555551234" doc:"Customer phone number"`
+	PreferredLocales []string          `json:"preferred_locales,omitempty" doc:"Customer's preferred languages"`
+	PromotionCode    *string           `json:"promotion_code,omitempty" doc:"Promotion code to apply"`
+	TaxExempt        *string           `json:"tax_exempt,omitempty" example:"none" doc:"Tax exemption status: none, exempt, or reverse"`
+}
+
+type CustomField struct {
+	Name  string `json:"name" example:"Tax ID" doc:"Field name"`
+	Value string `json:"value" example:"123-45-6789" doc:"Field value"`
+}
+
+type DeleteStripeCustomerInput struct {
+	ID uuid.UUID `json:"path" maxLength:"36" example:"1" doc:"ID of the user on stripe"`
+}
+
+type DeleteStripeCustomerResponse struct {
+	ID      string `gorm:"primaryKey;column:id" json:"id" example:"cus_NffrFeUfNV2Hib" doc:"Stripe customer ID"`
+	Object  string `gorm:"column:object" json:"object" example:"customer" doc:"Object type"`
+	Deleted bool   `gorm:"column:deleted" json:"deleted" example:"True" doc:"Object bool"`
+}
