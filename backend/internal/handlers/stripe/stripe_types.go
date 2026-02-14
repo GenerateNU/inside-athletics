036bd6009fb21735719b0292ff7ce5c57488e332
Hannah Bang <hannahbang@outlook.com>
2026-02-14 03:05:16 -0500

    removed response types from types

--- backend/internal/handlers/stripe/stripe_types.go
+++ backend/internal/handlers/stripe/stripe_types.go
@@ -71,36 +71,6 @@ type ArchiveStripePriceRequest struct {
 	ID string `json:"id" example:"price_123" doc:"ID of the product"`
 }
 
-type StripeProductResponse struct {
-	ID          string `json:"id" example:"price_123" doc:"ID of the product"`
-	Name        string `json:"name" binding:"required,min=1,max=100" example:"Premium Plan"`
-	Description string `json:"description" binding:"required,min=1,max=200" example:"Get premium content with this subscription"`
-}
-
-type StripePriceResponse struct {
-	ID            string   `json:"id" example:"price_123" doc:"ID of the product"`
-	UnitAmount    float32  `json:"total" example:"25.50" doc:"Price per billing cycle."`
-	Interval      Interval `json:"interval" example:"day" doc:"Interval between payments"`
-	IntervalCount int      `json:"interval_count" example:"3" doc:"Number of intervals a billing cycle lasts"`
-}
-
-func ToStripeProductResponse(stripe_product *models.StripeProduct) *StripeProductResponse {
-	return &StripeProductResponse{
-		ID:          stripe_product.ID,
-		Name:        stripe_product.Name,
-		Description: stripe_product.Description,
-	}
-}
-
-func ToStripePriceResponse(stripe_price *models.StripePrice) *StripePriceResponse {
-	return &StripePriceResponse{
-		ID:            stripe_price.ID,
-		UnitAmount:    stripe_price.UnitAmount,
-		Interval:      Interval(stripe_price.Interval),
-		IntervalCount: stripe_price.IntervalCount,
-	}
-}
-
 type GetStripeCustomerInput struct {
 	ID uuid.UUID `path:"id" maxLength:"36" example:"1" doc:"ID to identify the stripe user"`
 }
