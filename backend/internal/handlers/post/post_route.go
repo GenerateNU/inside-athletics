ad686f368a7b9a24a08a9cf43d8bb0fdf6ebb6b7
Logan Ravinuthala <loganravin4@gmail.com>
2026-03-16 00:21:00 -0400

    track post views, enforce free tier limits, checks for account type

--- backend/internal/handlers/post/post_route.go
+++ backend/internal/handlers/post/post_route.go
@@ -1,12 +1,15 @@
 package post
 
 import (
+	"inside-athletics/internal/handlers/user"
+
 	"github.com/danielgtaylor/huma/v2"
 	"gorm.io/gorm"
 )
 
 func Route(api huma.API, db *gorm.DB) {
-	postService := NewPostService(db)
+	userDB := user.NewUserDB(db)
+	postService := NewPostService(db, userDB)
 	{
 		grp := huma.NewGroup(api, "/api/v1/post")
 		huma.Post(grp, "/", postService.CreatePost)                 // Create post
