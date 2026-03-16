3092b0f40cf6fe6f0209ca282c518188d3053a43
ZainabImadulla <zainab.imadulla@icloud.com>
2026-03-16 13:09:17 -0400

    added openpi and auth config

---
+++ frontend/api/auth.ts
@@ -0,0 +1,72 @@
+import { createBrowserClient, createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export function createSupabaseClient() {
+    return createBrowserClient(
+        process.env.NODE_ENV === "production"
+            ? process.env.NEXT_PUBLIC_SUPABASE_URL!
+            : process.env.NEXT_PUBLIC_DEV_SUPABASE_URL!,
+        process.env.NODE_ENV === "production"
+            ? process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+            : process.env.NEXT_PUBLIC_SUPABASE_DEV_PUBLISHABLE_KEY!
+    );
+}
+
+export async function createSupabaseServerClient() {
+    const cookieStore = await cookies();
+    return createServerClient(
+        process.env.NODE_ENV === "production"
+            ? process.env.NEXT_PUBLIC_SUPABASE_URL!
+            : process.env.NEXT_PUBLIC_DEV_SUPABASE_URL!,
+        process.env.NODE_ENV === "production"
+            ? process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+            : process.env.NEXT_PUBLIC_SUPABASE_DEV_PUBLISHABLE_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return cookieStore.getAll();
+                },
+                setAll(cookiesToSet) {
+                    try {
+                        cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+                    } catch {}
+                },
+            },
+        }
+    );
+}
+
+/**
+ * Server-side: Creates the Authorization Header for server-side api calls
+ */
+export async function getServerAuthorizationHeader(): Promise<HeadersInit> {
+    const supabase = await createSupabaseServerClient();
+    const { data } = await supabase.auth.getSession();
+    if (!data.session?.access_token) {
+        throw new Error("Authorization token is missing.");
+    }
+    return {
+        Authorization: `Bearer ${data.session?.access_token}`
+    }
+}
+
+/**
+ * Client-side: Creates the Authorization Header for client-side api calls
+ */
+export async function getClientAuthorizationHeader(): Promise<HeadersInit> {
+    const supabase = createSupabaseClient();
+    ("@ts-expect-error");
+    const { data, error } = await supabase.auth.getSession();
+
+    if (error) {
+        throw new Error(`Failed to get session: ${error.message}`);
+    }
+
+    if (!data.session?.access_token) {
+        throw new Error("Authorization token is missing.");
+    }
+
+    return {
+        Authorization: `Bearer ${data.session?.access_token}`
+    }
+}
