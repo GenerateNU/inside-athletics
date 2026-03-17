01a4ac4c8553cff6fe93ab573b7669eb4e4d61f4
Hannah Bang <hannahbang@outlook.com>
2026-03-16 21:44:19 -0400

    video_route

---
+++ backend/internal/handlers/video/video_route.go
@@ -0,0 +1,16 @@
+package post
+
+import (
+	"github.com/danielgtaylor/huma/v2"
+	"gorm.io/gorm"
+)
+
+func Route(api huma.API, db *gorm.DB) {
+	videoService := NewVideoService(db)
+	{
+		grp := huma.NewGroup(api, "/api/v1/video")
+		huma.Post(grp, "/", videoService.CreatePost)                 // Add video
+		huma.Get(grp, "/{id}", videoService.GetPostByID)             // Get video by id
+		huma.Delete(grp, "/{id}", videoService.DeletePost)           // Delete video
+	}
+}
