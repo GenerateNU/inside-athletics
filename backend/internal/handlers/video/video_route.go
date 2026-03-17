6e1cead88f24b6e668973919e68b55a243214954
Avni Mishra <avni.mishra0409@gmail.com>
2026-03-16 21:45:36 -0400

    adding package video

--- backend/internal/handlers/video/video_route.go
+++ backend/internal/handlers/video/video_route.go
@@ -1,4 +1,4 @@
-package post
+package video
 
 import (
 	"github.com/danielgtaylor/huma/v2"
@@ -9,8 +9,8 @@ func Route(api huma.API, db *gorm.DB) {
 	videoService := NewVideoService(db)
 	{
 		grp := huma.NewGroup(api, "/api/v1/video")
-		huma.Post(grp, "/", videoService.CreatePost)                 // Add video
-		huma.Get(grp, "/{id}", videoService.GetPostByID)             // Get video by id
-		huma.Delete(grp, "/{id}", videoService.DeletePost)           // Delete video
+		huma.Post(grp, "/", videoService.CreatePost)       // Add video
+		huma.Get(grp, "/{id}", videoService.GetPostByID)   // Get video by id
+		huma.Delete(grp, "/{id}", videoService.DeletePost) // Delete video
 	}
 }
