41cd5245c731e4085849e51b8e84cd2a3b239513
Zahra Wibisana <wibisana.z@northeastern.edu>
2026-03-23 14:09:26 -0400

    Added model for premium posts

---
+++ backend/internal/handlers/premium_post/premium_post_service.go
@@ -0,0 +1,6 @@
+package premiumpost
+
+// --- reminders
+// add permission to only allow moderators and admins for access to get, create, update, delete
+// add permission so that only paid users can view the posts
+
