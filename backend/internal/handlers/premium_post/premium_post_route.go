b9ef3e7fcb818759fcd9aabf412e703a51e45931
Zahra Wibisana <wibisana.z@northeastern.edu>
2026-03-23 21:13:23 -0400

    Updated create endpoint

--- backend/internal/handlers/premium_post/premium_post_route.go
+++ backend/internal/handlers/premium_post/premium_post_route.go
@@ -1,1 +1,15 @@
 package premiumpost
+
+import (
+	"github.com/danielgtaylor/huma/v2"
+	"gorm.io/gorm"
+)
+
+func Route(api huma.API, db *gorm.DB) {
+	var premiumPostDB = &PremiumPostDB{db: db}
+	var premiumPostService = &PremiumPostService{premiumPostDB}
+	{
+		grp := huma.NewGroup(api, "/api/v1/post/premium")
+		huma.Post(grp, "/", premiumPostService.CreatePremiumPost)                 // Create post
+	}
+}
