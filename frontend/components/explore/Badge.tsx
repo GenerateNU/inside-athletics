5e7d275bd2496de225ef936f299c4161b460f771
ZainabImadulla <zainab.imadulla@icloud.com>
2026-04-14 20:09:20 -0400

    Fix Button Styling

--- frontend/components/explore/Badge.tsx
+++ frontend/components/explore/Badge.tsx
@@ -16,7 +16,7 @@ export function Badge({ icon, count, onClick, active, className }: BadgeProps) {
       disabled={!onClick}
       className={cn(
         "flex items-center gap-5 rounded-full border border-[#3E7DBB] bg-white px-2 w-17 py-2 text-xs text-zinc-500 transition-colors text-md",
-        onClick && "hover:border-red-200 hover:bg-red-50 hover:text-red-500",
+        onClick && "hover:border-[#043D26] hover:text-[#043D26]",
         !onClick && "cursor-default",
         active && "text-red-500",
         className,
